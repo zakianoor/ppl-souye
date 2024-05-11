106d960642b3ace81b6b1f50ad7d80c728faf08a
zakianoor <86943025+zakianoor@users.noreply.github.com>
2024-05-11 22:18:56 +0700

    laravel souye

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/navbar', function () {
+    return view('navbar');
+});
+
+Route::get('/footer', function () {
+    return view('footer');
+});
+
+Route::get('/base', function () {
+    return view('base');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
